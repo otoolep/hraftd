efdea59e7a7a922d5ed0fbc54a5f738a161e681e
Philip O'Toole <philip.otoole@yahoo.com>
2015-10-04 23:35:19 -0700

    Create temp dirs for store testing

--- store/store_test.go
+++ store/store_test.go
@@ -1,11 +1,18 @@
 package store
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
 func Test_StoreOpen(t *testing.T) {
 	s := New()
+	tmpDir, _ := ioutil.TempDir("", "store_test")
+	defer os.RemoveAll(tmpDir)
+
 	s.RaftBind = "127.0.0.1:8088"
-	s.RaftDir = "/tmp/raft"
+	s.RaftDir = tmpDir
 	if s == nil {
 		t.Fatalf("failed to create store")
 	}
