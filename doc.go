3a9c38c42a0fc66cf4caee5faae318170d6bd7da
Philip O'Toole <philip.otoole@yahoo.com>
2015-10-10 16:15:56 -0700

    Fix godoc typos

--- doc.go
+++ doc.go
@@ -1,3 +1,3 @@
 // Package hraftd is a simple distributed key-value store, acting
-// as a reference use of the Raft implemenation from Hashicorp.
+// as a reference use of the Raft implementation from Hashicorp.
 package hraftd
