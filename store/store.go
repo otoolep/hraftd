400e9b179014e977b1d2a8bf22ce103c9a75b55e
Philip O'Toole <philip.otoole@yahoo.com>
2015-10-11 21:02:51 -0700

    Implement snapshot support

--- store/store.go
+++ store/store.go
@@ -177,11 +177,32 @@ func (f *fsm) Apply(l *raft.Log) interface{} {
 
 // Snapshot returns a snapshot of the key-value store.
 func (f *fsm) Snapshot() (raft.FSMSnapshot, error) {
-	return nil, nil
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	// Clone the map.
+	o := make(map[string]string)
+	for k, v := range f.m {
+		o[k] = v
+	}
+	return &fsmSnapshot{store: o}, nil
 }
 
 // Restore stores the key-value store to a previous state.
 func (f *fsm) Restore(rc io.ReadCloser) error {
+	b, err := ioutil.ReadAll(rc)
+	if err != nil {
+		return err
+	}
+
+	o := make(map[string]string)
+	if err := json.Unmarshal(b, &o); err != nil {
+		return err
+	}
+
+	// Set the state from the snapshot, no lock required according to
+	// Hashicorp docs.
+	f.m = o
 	return nil
 }
 
@@ -198,6 +219,41 @@ func (f *fsm) applyDelete(key string) interface{} {
 	delete(f.m, key)
 	return nil
 }
+
+type fsmSnapshot struct {
+	store map[string]string
+}
+
+func (f *fsmSnapshot) Persist(sink raft.SnapshotSink) error {
+	err := func() error {
+		// Encode data.
+		b, err := json.Marshal(f.store)
+		if err != nil {
+			return err
+		}
+
+		// Write data to sink.
+		if _, err := sink.Write(b); err != nil {
+			return err
+		}
+
+		// Close the sink.
+		if err := sink.Close(); err != nil {
+			return err
+		}
+
+		return nil
+	}()
+
+	if err != nil {
+		sink.Cancel()
+		return err
+	}
+
+	return nil
+}
+
+func (f *fsmSnapshot) Release() {}
 
 func readPeersJSON(path string) ([]string, error) {
 	b, err := ioutil.ReadFile(path)
