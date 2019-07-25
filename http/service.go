76585df88eea32dacfc9c51d05103b19b155a32e
mike <liyy@yunphant.com>
2019-07-25 17:40:27 +0800

    remove exit func

--- http/service.go
+++ http/service.go
@@ -24,9 +24,6 @@ type Store interface {
 
 	// Join joins the node, identitifed by nodeID and reachable at addr, to the cluster.
 	Join(nodeID string, addr string) error
-
-	// Exit exits the node
-	Exit(nodeID string, addr string) error
 }
 
 // Service provides HTTP service.
@@ -81,8 +78,6 @@ func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		s.handleKeyRequest(w, r)
 	} else if r.URL.Path == "/join" {
 		s.handleJoin(w, r)
-	}else if r.URL.Path == "/exit" {
-		s.handleExit(w, r)
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 	}
@@ -113,36 +108,6 @@ func (s *Service) handleJoin(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := s.store.Join(nodeID, remoteAddr); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-}
-
-func (s *Service) handleExit(w http.ResponseWriter, r *http.Request) {
-	m := map[string]string{}
-	if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if len(m) != 2 {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	remoteAddr, ok := m["addr"]
-	if !ok {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	nodeID, ok := m["id"]
-	if !ok {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if err := s.store.Exit(nodeID, remoteAddr); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
--- test.sh
+++ test.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+
+go build
+
+#启动leader节点
+$GOPATH/bin/hraftd -id node0  ~/node0
+
+#启动node1
+$GOPATH/bin/hraftd -id node1 -haddr :11001 -raddr :12001 -join :11000 ~/node1
 
 #只能通过leader的http端口来进行set
 curl -XPOST localhost:11000/key -d '{foo: bar}'
