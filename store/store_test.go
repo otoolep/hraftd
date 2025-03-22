f18d993308523f62e657833a476757f349ba4868
Aza Maulana <azamaulanaaa@gmail.com>
2025-03-23 04:53:54 +0700

    test: add store status test case
    - test store status `me` has correct value
    - test any node must not be a leader and a follower at same time
    - test `me` me be a leader or a follower

--- store/store_test.go
+++ store/store_test.go
@@ -120,3 +120,51 @@ func Test_StoreInMemOpenSingleNode(t *testing.T) {
 		t.Fatalf("key has wrong value: %s", value)
 	}
 }
+
+func Test_StoreStatus(t *testing.T) {
+	s := New(false)
+	tmpDir, _ := ioutil.TempDir("", "store_test")
+	defer os.RemoveAll(tmpDir)
+
+	s.RaftBind = "127.0.0.1:0"
+	s.RaftDir = tmpDir
+	if s == nil {
+		t.Fatalf("failed to create store")
+	}
+
+	if err := s.Open(true, "node0"); err != nil {
+		t.Fatalf("failed to open store: %s", err)
+	}
+
+	// assuming 3 seconds enough for raft to initialized
+	time.Sleep(3 * time.Second)
+
+	status, err := s.Status()
+	if err != nil {
+		t.Errorf("failed to get store status: %s", err)
+	}
+
+	if status.Me.ID != "node0" {
+		t.Errorf("status `me.id` has invalid value")
+	}
+	if status.Me.Address != s.RaftBind {
+		t.Errorf("status `me.address` has invalid value")
+	}
+
+	for _, follower := range status.Followers {
+		if (follower.ID == status.Leader.ID) || (follower.Address == status.Leader.Address) {
+			t.Fatalf("a node cannot be leader and follow at the same time")
+		}
+	}
+
+	isMeInFollowersOrLeader := false
+	for _, node := range append(status.Followers, status.Leader) {
+		if node.ID == status.Me.ID && node.Address == status.Me.Address {
+			isMeInFollowersOrLeader = true
+			break
+		}
+	}
+	if !isMeInFollowersOrLeader {
+		t.Errorf("me must be exist exclusively as a leader or as a follower")
+	}
+}
